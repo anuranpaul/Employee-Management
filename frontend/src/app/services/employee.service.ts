0af560d1a0fc8837b4999d2c4926ea56bfa921f4
Anuran Paul <anuran34@gmail.com>
2024-07-29 10:22:44 +0530

    Added features

--- frontend/src/app/services/employee.service.ts
+++ frontend/src/app/services/employee.service.ts
@@ -21,6 +21,11 @@ export class EmployeeService {
     return this.http.get<any[]>(`${this.apiUrl}/view`);
   }
 
+  getEmployeeById(employeeId : string): Observable<any> {
+    console.log('Fetching employee with id: ', employeeId);
+    return this.http.get<any>(`${this.apiUrl}/view/${employeeId}`);
+  }
+
   updateEmployee(employee : any){
     console.log('Updating employee: ', employee);
     return this.http.put<any>(`${this.apiUrl}/update`, employee);
